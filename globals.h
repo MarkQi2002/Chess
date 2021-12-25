f9a09c68c60b3e82b03521303292b87d21bf4eb6
MarkQi2002 <qiji2002714@gmail.com>
2021-12-24 19:05:56 -0500

    Updated main.cpp, globals.h, and added handout

--- globals.h
+++ globals.h
@@ -1,4 +1,27 @@
 #ifndef _globals_h
 #define _globals_h
 
+// Include Modules
+#include <string>
+
+// Variable Declaration
+// Check PDF Handout For More Information
+// Pieces, Black (Lower Case Letters), White (Capital Letters)
+#define whiteKing "K"
+#define whiteQueen "Q"
+#define whiteBishop "B"
+#define whiteRook "R"
+#define whiteKnight "N"
+#define whitePawn "P"
+
+#define blackKing "k"
+#define blackQueen "q"
+#define blackBishop "b"
+#define blackRook "r"
+#define blackKnight "n"
+#define blackPawn "p"
+
+// Board Related Declaration
+#define boardSize 8
+
 #endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+// Include Modules
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+#include "globals.h"
+
+// The Main Function
 int main(){
     cout << "Hello World" << endl;
     
